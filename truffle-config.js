6a5ac6a44ebdb3d5e41c81b794d9b369e7bf12cf
sreeharshakaza <sriharsha.kaza@gmail.com>
2021-11-20 11:19:19 +0530

    Configured to deploy on Harmony testnet.

--- truffle-config.js
+++ truffle-config.js
@@ -1,12 +1,29 @@
 require('babel-register');
 require('babel-polyfill');
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const mnemonic = '0x4CC7F1815F7538e1792DC4E86f21d11C0aAC241d';
+const privateKeyTest = 'd20f32bd53c439f86b80e31c74d3b344274daf8bcef6111d1e7177c10d392ce4';
 
 module.exports = {
   networks: {
-    development: {
-      host: "127.0.0.1",
-      port: 7545,
-      network_id: "*" // Match any network id
+    testnet: {
+      provider: () => new HDWalletProvider(privateKeyTest, 'https://api.s0.b.hmny.io'),
+      network_id: 1666700000,
+      skipDryRun: true,
+    },
+    testnetHar: {
+      provider: () => {
+        if (!privateKeyTest.trim()) {
+          throw new Error(
+            'Please enter a private key with funds, you can use the default one'
+          );
+        }
+        return new HDWalletProvider({
+          privateKeys: [privateKeyTest],
+          providerOrUrl: 'https://api.s0.b.hmny.io',
+        });
+      },
+      network_id: 1666700000,
     },
   },
   contracts_directory: './src/contracts/',
@@ -20,3 +37,5 @@ module.exports = {
     }
   }
 }
+
+
