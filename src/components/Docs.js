8bee16c7885185d356bfbd1a4cfdd980244b6097
saidutt chakka <saidutt@gmail.com>
2022-01-04 10:13:56 +0530

    Doc.js title and content padding fix

--- src/components/Docs.js
+++ src/components/Docs.js
@@ -7,9 +7,8 @@ import Navbar from "./Navbar";
 import { DefaultPlayer as Video } from "react-html5video";
 import "react-html5video/dist/styles.css";
 import { useCookies } from "react-cookie";
-import ReactMarkdown from 'react-markdown';
-import remarkGfm from 'remark-gfm';
-
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
 
 const Docs = () => {
   const [title, setTitle] = useState([]);
@@ -34,30 +33,28 @@ const Docs = () => {
     const pageContentAPI = `https://static.kulfyapp.com/blogs?url=${keyword}`;
 
     axios.defaults.headers.common = {
-      "Content-Type": "application/json"
+      "Content-Type": "application/json",
     };
 
     const getPageResponse = await axios.get(`${pageContentAPI}`);
 
     setTitle(getPageResponse.data[0].title);
     setBody(getPageResponse.data[0].body);
-
   }
 
   async function createMeme(item) {
     localStorage.setItem("NFT", JSON.stringify(item));
-    window.location.href =
-      "/creator?nft=" + item.cached_file_url;
+    window.location.href = "/creator?nft=" + item.cached_file_url;
   }
 
   return (
     <>
       <Navbar />
-      {title}
-      <section class="container featured-grid">
-      <div class="row d-flex justify-content-between">
-         <ReactMarkdown children={body} remarkPlugins={[remarkGfm]} />
-         </div>
+      <h1 className="text-center">{title}</h1>
+      <section class="container featured-grid mb-5">
+        <div class="row d-flex justify-content-between p-3">
+          <ReactMarkdown children={body} remarkPlugins={[remarkGfm]} />
+        </div>
       </section>
     </>
   );
