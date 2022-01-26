30475f849dc4e89f102b696d133cf229235bbf45
saidutt chakka <saidutt@gmail.com>
2022-01-26 20:58:28 +0530

    tip pop styling

--- src/components/ModelPopUp.js
+++ src/components/ModelPopUp.js
@@ -1,94 +1,113 @@
 import React, { Component } from "react";
 import "./App.css";
-import { Modal } from 'react-bootstrap';
+import { Modal } from "react-bootstrap";
 import KulfyV3 from "../abis/KulfyV3.json";
 
 export class ModelPopUp extends Component {
-    isShowModal(state)
-    {
-      this.setState({ showModalPopup: state });
-    }
-    updateInputitemValue(evt) {
-    
-      this.setState({
-        inputItem: evt.target.value
+  isShowModal(state) {
+    this.setState({ showModalPopup: state });
+  }
+  updateInputitemValue(evt) {
+    this.setState({
+      inputItem: evt.target.value,
+    });
+  }
+  setInputitemValue(val) {
+    this.setState({
+      inputItem: val,
+    });
+  }
+  updateInputTipValue(evt) {
+    this.setState({
+      inputTip: evt.target.value,
+    });
+  }
+  async tipKulfyAuthor(id, tipAmount) {
+    console.log(id);
+    this.isShowModal(false);
+    this.state.kulfyV3.methods
+      .tipKulfyAuthor(id, 5000)
+      .send({
+        from: this.state.account,
+        value: window.web3.utils.toWei(tipAmount, "Ether"),
+      })
+      .on("transactionHash", (hash) => {
+        console.log("tans hash ", hash);
       });
-    }
-    setInputitemValue (val) {
-    
-        this.setState({
-          inputItem: val
-        });
-      }
-    updateInputTipValue(evt) {
-   
-      this.setState({
-        inputTip:  evt.target.value
-      });
-    }
-    async tipKulfyAuthor(id, tipAmount) {
-        console.log(id);
-        this.isShowModal(false)
-        this.state.kulfyV3.methods
-          .tipKulfyAuthor(id,5000)
-          .send({
-            from: this.state.account,
-            value: window.web3.utils.toWei(tipAmount, "Ether"),
-          })
-          .on("transactionHash", (hash) => {
-            console.log("tans hash ", hash);
-          });
-      }
-      constructor(props) {
-        super(props);
-        this.state = {
-          account: this.props.account,
-          showModalPopup: this.props.showModalPopup,
-          inputTip:"1",
-          kulfyV3: this.props.kulfyV3,
-          inputItem:""
-        };
-    }
+  }
+  constructor(props) {
+    super(props);
+    this.state = {
+      account: this.props.account,
+      showModalPopup: this.props.showModalPopup,
+      inputTip: "1",
+      kulfyV3: this.props.kulfyV3,
+      inputItem: "",
+    };
+  }
   render() {
     return (
       <>
-        
-      <Modal style={{color:'#000'}} show={this.state.showModalPopup} onHide={this.handleClose}  
-                    size="lg"  
-                    aria-labelledby="contained-modal-title-vcenter"  
-                    centered  
-                >  
-                    <Modal.Header>  
-                        <Modal.Title id="sign-in-title">  
-                            Add Tip 
-                         </Modal.Title>  
-                    </Modal.Header>  
-                    <Modal.Body>  
-                         
-                        <div className="signUp"> 
-                        <input type="hidden" value={this.state.inputItem} onChange={evt => this.updateInputitemValue(evt)}  />
-                       
-                        <input type="number" value={this.state.inputTip} onChange={evt => this.updateInputTipValue(evt)} placeholder="1" style={{marginRight:'10px', width : '80px'}}  required name="price"  min=".1" step="0.1" title="Currency" /> ONE
-                        <div style={{marginTop:'20px'}}>
-                        <a
-                          href="#"
-                          className="btn-create "
-                          onClick={() => this.tipKulfyAuthor(this.state.inputItem,this.state.inputTip)}
-                        >
-                          Tip
-                        </a>  
-                            <a
-                          href="#" style={{marginLeft:'10px'}}
-                          className="btn-create "
-                          onClick={() => this.isShowModal(false)}
-                        >
-                          Cancel
-                        </a>
-                        </div>
-                        </div>  
-                    </Modal.Body>  
-  
-                </Modal >  
+        <Modal
+          style={{ color: "#000" }}
+          show={this.state.showModalPopup}
+          onHide={this.handleClose}
+          size="md"
+          aria-labelledby="contained-modal-title-vcenter"
+          centered
+        >
+          <Modal.Header>
+            <Modal.Title id="sign-in-title">
+              <h1>Add Tip</h1>
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <div className="tip-pop">
+              <div className="my-4">
+                <input
+                  type="hidden"
+                  value={this.state.inputItem}
+                  onChange={(evt) => this.updateInputitemValue(evt)}
+                />
+                <input
+                  type="number"
+                  value={this.state.inputTip}
+                  onChange={(evt) => this.updateInputTipValue(evt)}
+                  placeholder="1"
+                  className="input-text-radium"
+                  required
+                  name="price"
+                  min=".1"
+                  step="0.1"
+                  title="Currency"
+                />{" "}
+                <span className="text-color">ONE</span>
+              </div>
+              <div className="my-4 d-flex">
+                <a
+                  href="#"
+                  className="btn-radium"
+                  onClick={() =>
+                    this.tipKulfyAuthor(
+                      this.state.inputItem,
+                      this.state.inputTip
+                    )
+                  }
+                >
+                  Tip
+                </a>
+                <a
+                  href="#"
+                  style={{ marginLeft: "10px" }}
+                  className="btn-radium-outline "
+                  onClick={() => this.isShowModal(false)}
+                >
+                  Cancel
+                </a>
+              </div>
+            </div>
+          </Modal.Body>
+        </Modal>
       </>
     );
   }
