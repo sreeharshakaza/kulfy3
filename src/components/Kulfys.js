e92b090c850b8ecae016ac90727cf6eb7392cfac
iabhishek79 <abhi.mondal17@gmail.com>
2022-03-28 20:41:11 +0530

    Fixed the loading data issue

--- src/components/Kulfys.js
+++ src/components/Kulfys.js
@@ -55,11 +55,11 @@ class Kulfys extends Component {
     //Network ID
     const networkId = await web3.eth.net.getId();
     const networkData = KulfyV3.networks[networkId];
+    console.log(networkId)
     if (networkData) {
       const kulfyV3 = new web3.eth.Contract(KulfyV3.abi, networkData.address);
       this.setState({ kulfyV3 });
       const kulfiesCount = await kulfyV3.methods.tokenIds().call();
-      console.log("Kulfy Count"+ kulfiesCount);
       this.setState({ kulfiesCount });
         if(this.state.intialCount<=kulfiesCount)
         {
@@ -79,7 +79,7 @@ class Kulfys extends Component {
               }            
           }
         this.setState({intialCount:this.state.intialCount+this.state.takeCount})
-        if((kulfiesCount-(this.state.intialCount+this.state.takeCount))<=1)
+        if((kulfiesCount-(this.state.intialCount+this.state.takeCount))<=0)
         this.setState({ hasmore: false });
       }
        else
@@ -142,7 +142,7 @@ class Kulfys extends Component {
       inputTip:"1",
       inputItem:"",
       intialCount:1,
-      takeCount:2,
+      takeCount:4,
       kulfyTotalCount:0,
       hasmore:true,
     };
